e9e9f9cfc85ad6a6da2fa1047edafbf61e1418ae
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-10 12:08:14 -0300

    Creating the winning modal component

---
+++ src/components/WinnerModal.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+import { Square } from "./Square";
+
+export function WinnerModal({ winner, restartGame }) {
+  if (winner === null) return null;
+
+  const winnerText = winner === false ? "It's a draw" : `The winner is: `;
+  return (
+    <section className="winner">
+      <div className="text">
+        <h2>{winnerText}</h2>
+
+        <header className="win">{winner && <Square>{winner}</Square>}</header>
+
+        <footer>
+          <button onClick={restartGame}>Play again</button>
+        </footer>
+      </div>
+    </section>
+  );
+}
