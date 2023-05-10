624ffedfbecb44ca9bbb1d6900b31e0e714df442
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-10 12:08:44 -0300

    Setting up the main board with its logic implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Square } from "./components/Square.jsx";
+import { TURNS } from "./constants.js";
+import { checkWinner, checkEndGame } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal.jsx";
+import confetti from "canvas-confetti";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setwinner] = useState(null);
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
+    setBoard([...board.slice(0, index), turn, ...board.slice(index + 1)]);
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti();
+      setwinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setwinner(false);
+    }
+  };
+
+  const restartGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setwinner(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>
+        The <span>best </span>Tic Tac Toe game on planet Earth
+      </h1>
+      <main className="board">
+        <button onClick={restartGame}>Restart</button>
+        <section className="game">
+          {board.map((_, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            );
+          })}
+        </section>
+
+        <section className="turn">
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        </section>
+
+        <WinnerModal restartGame={restartGame} winner={winner} />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
