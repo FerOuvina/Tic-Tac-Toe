1f815f7e228fad3a4e11bba3c9b42e88fb4f5d31
Fernando Ouviña <fernandoouvina07@gmail.com>
2023-05-11 11:20:12 -0300

    Creating footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        Made by{" "}
+        <a href="https://github.com/FerOuvina" target="_blank" rel="noreferrer" className="footer__link">
+          Fernando Ouvi&#241;a
+        </a>{" "}
+        | 2023
+      </p>
+    </footer>
+  );
+}
